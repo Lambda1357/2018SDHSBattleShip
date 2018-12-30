a7db7f4a5a1243004b37966fbe889df339eff473
Junhyeong Bae <bjh6681@outlook.kr>
2018-12-30 15:00:50 +0900

    Add inline keyword (=fix compile error)

--- Lerf.h
+++ Lerf.h
@@ -1,13 +1,13 @@
 #pragma once
 
 //1차원 선형 보간. a와 b사이의 값들 중, 수직선상 t(0.0 ~ 1.0) 지점에 있는 값을 추정하여 반환한다.
-float Lerp(float a, float b, float t)
+inline float Lerp(float a, float b, float t)
 {
 	return float(1 - t)*a + (t*b);
 }
 
 //3차원 선형 보간. min 벡터와 max 벡터의 대응 축마다 1차원 선형보간하여 보간된 벡터를 만든다.
-D3DXVECTOR3 Lerp(D3DXVECTOR3 departure, D3DXVECTOR3 destination, float t)
+inline D3DXVECTOR3 Lerp(D3DXVECTOR3 departure, D3DXVECTOR3 destination, float t)
 {
 	D3DXVECTOR3 result = { Lerp(departure.x,destination.x,t),Lerp(departure.y,destination.y,t),Lerp(departure.z,destination.z,t) };
 	return result;
